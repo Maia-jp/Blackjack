b62d64be1701bc9d8ac766f9b482c770a8868702
Lucas Xavier <lucasxaviert875@gmail.com>
2021-04-28 15:11:30 -0300

    Inserção dos método pagar e receber na classe Jogador

--- src/blackjack/model/Impl/Jogador.java
+++ src/blackjack/model/Impl/Jogador.java
@@ -2,12 +2,12 @@ package blackjack.model.Impl;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class Jogador {
 	private String nomeJogador;
-	private Map <String, Integer> fichasJogador = new HashMap<String, Integer>();
+	private LinkedHashMap <String, Integer> fichasJogador = new LinkedHashMap<String, Integer>();
 	private List<Baralho> cartasJogador;
 	
 	public Jogador(String nome) {
@@ -23,18 +23,18 @@ public class Jogador {
 		this.nomeJogador = nomeJogador;
 	}
 	
-	public Map <String, Integer> getFichasJogador() {
+	public LinkedHashMap <String, Integer> getFichasJogador() {
 		return fichasJogador;
 	}
 
-	public void setFichasJogador(Map <String, Integer> fichasJogador) {
+	public void setFichasJogador(LinkedHashMap <String, Integer> fichasJogador) {
 		this.fichasJogador.put("100", 2);
 		this.fichasJogador.put("50", 2);
 		this.fichasJogador.put("20", 5);
 		this.fichasJogador.put("10", 5);
 		this.fichasJogador.put("5", 8);
 		this.fichasJogador.put("1", 10);
-	
+		
 	}
 	
 	public List<Baralho> getCartasJogador() {
@@ -45,5 +45,25 @@ public class Jogador {
 		this.cartasJogador = new ArrayList<>();
 	}
 
+	public void receberFichas(int quantia) {
+		for (Map.Entry<String, Integer> entry : this.fichasJogador.entrySet()) {
+		    this.fichasJogador.replace(entry.getKey(),entry.getValue()+quantia/Integer.parseInt(entry.getKey()));
+		    quantia=quantia%Integer.parseInt(entry.getKey());
+		    if(quantia==0){
+		        break;
+		    }
+        }
+	}
+	
+	//aposta minima de $20 e máxima de $100
+	public void pagarFichas(int aposta) {
+		for (Map.Entry<String, Integer> entry : this.fichasJogador.entrySet()) {
+		    this.fichasJogador.replace(entry.getKey(),entry.getValue()-aposta/Integer.parseInt(entry.getKey()));
+		    aposta=aposta%Integer.parseInt(entry.getKey());
+		    if(aposta==0){
+		        break;
+		    }
+        }
+	}
 	
 }
