904ac4a0b8aae72f626fc68cfd78e35e8e4e8ede
Alexandre R. Bomfim Junior <abjbomfim@outlook.com>
2021-05-14 20:11:22 -0300

    Nova atualização View
    Corrigindo o erro da ultima issue#25
    Tela jogador

--- src/blackjack/view/TelaBanca.java
+++ src/blackjack/view/TelaBanca.java
@@ -14,21 +14,23 @@ import javax.swing.JFrame;
 
 public class TelaBanca extends JFrame{
 	private final int LARG_DEFAULT=891;
-	private final int ALT_DEFAULT=707;
+	private final int ALT_DEFAULT=700;
 	public TelaBanca(CarregaImagens cI, List<String> m, int valorDealer) {
 		inicializar(cI, m);
 	}
 	private void inicializar(CarregaImagens cI, List<String> m) {
 		Toolkit tk=Toolkit.getDefaultToolkit();
 		Dimension screenSize=tk.getScreenSize();
+		setResizable(false);
 		int sl=screenSize.width;
 		int sa=screenSize.height;
 		int x=sl/2-LARG_DEFAULT/2;
 		int y=sa/2-ALT_DEFAULT/2;
-		setBounds(x,y,LARG_DEFAULT,ALT_DEFAULT);
+		setBounds(x+100,y,LARG_DEFAULT,ALT_DEFAULT);
 		ImagensTela s = new ImagensTela(cI, m);
         add(s);
         s.repaint();
+        
 	}
 	
 }
--- src/blackjack/view/TelaJogador.java
+++ src/blackjack/view/TelaJogador.java
@@ -1,6 +1,5 @@
 package blackjack.view;
 
-import java.awt.Canvas;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Label;
@@ -18,71 +17,78 @@ public class TelaJogador extends JFrame {
 	private void initialize() {
 		this.getContentPane().setBackground(Color.DARK_GRAY);
 		this.getContentPane().setLayout(null);
+		this.setResizable(false);
 		
+	
 		Panel panel = new Panel();
 		panel.setBackground(Color.WHITE);
-		panel.setBounds(729, 10, 195, 133);
+		panel.setBounds(10, 10, 713, 641);
 		this.getContentPane().add(panel);
+		this.setBounds(0, 50, 950, 700);
 		panel.setLayout(null);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		Panel panelButton = new Panel();
-		panelButton.setBackground(Color.WHITE);
-		panelButton.setBounds(729, 160, 195, 133);
-		this.getContentPane().add(panelButton);
-		panelButton.setLayout(null);
+		Panel panel1 = new Panel();
+		panel1.setBackground(Color.WHITE);
+		panel1.setBounds(729, 10, 195, 133);
+		this.getContentPane().add(panel1);
+		panel1.setLayout(null);
+		
+		Panel panel2 = new Panel();
+		panel2.setBackground(Color.WHITE);
+		panel2.setBounds(729, 518, 195, 133);
+		this.getContentPane().add(panel2);
+		panel2.setLayout(null);
 		
 		Label label = new Label("Nome do Jogador");
 		label.setAlignment(Label.CENTER);
 		label.setBounds(45, 10, 98, 22);
-		panel.add(label);
+		panel1.add(label);
 		
 		Label label_1 = new Label("Aposta total: ");
 		label_1.setAlignment(Label.CENTER);
 		label_1.setBounds(10, 38, 98, 22);
-		panel.add(label_1);
+		panel1.add(label_1);
 		
 		Label label_1_1 = new Label("Dinheiro Total: ");
 		label_1_1.setAlignment(Label.CENTER);
 		label_1_1.setBounds(10, 66, 98, 22);
-		panel.add(label_1_1);
+		panel1.add(label_1_1);
 		
-		JButton jb = new JButton("STAND");
-		jb.setBounds(50,9,100,25);
+		Label label_1_1_1 = new Label("Valor da mão");
+		label_1_1_1.setAlignment(Label.CENTER);
+		label_1_1_1.setBounds(45, 10, 98, 22);
+		panel2.add(label_1_1_1);
+		
+		JButton jb = new JButton("HIT");
+		jb.setBounds(118,597,100,33);
 		jb.setFont(new Font("Helvetica", Font.BOLD, 15));
 		jb.setForeground(Color.DARK_GRAY);
 		jb.setBackground(Color.LIGHT_GRAY);
-		panelButton.add(jb);
+		panel.add(jb);
 		
-		JButton jb2 = new JButton("HIT");
-		jb2.setBounds(50,39,100,25);
+		JButton jb2 = new JButton("STAND");
+		jb2.setBounds(238,597,100,33);
 		jb2.setFont(new Font("Helvetica", Font.BOLD, 15));
 		jb2.setForeground(Color.DARK_GRAY);
 		jb2.setBackground(Color.LIGHT_GRAY);
-		panelButton.add(jb2);
+		panel.add(jb2);
 		
 		JButton jb3 = new JButton("DOUBLE");
-		jb3.setBounds(50,69,100,25);
+		jb3.setBounds(358,597,100,33);
 		jb3.setFont(new Font("Helvetica", Font.BOLD, 15));
 		jb3.setForeground(Color.DARK_GRAY);
 		jb3.setBackground(Color.LIGHT_GRAY);
-		panelButton.add(jb3);
+		panel.add(jb3);
 		
 		JButton jb4 = new JButton("SPLIT");
-		jb4.setBounds(50,99,100,25);
+		jb4.setBounds(478,597,100,33);
 		jb4.setFont(new Font("Helvetica", Font.BOLD, 15));
 		jb4.setForeground(Color.DARK_GRAY);
 		jb4.setBackground(Color.LIGHT_GRAY);
-		panelButton.add(jb4);
+		panel.add(jb4);
 		
-		Canvas canvas = new Canvas();
-		canvas.setBackground(Color.WHITE);
-		canvas.setBounds(10, 10, 713, 641);
-		this.getContentPane().add(canvas);
-		this.setBounds(100, 100, 950, 700);
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
 	}
-	
-	
-	
 	
 }
